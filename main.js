7994e8c4f282257efaa41d09d704e00484e74685
WaleriiChirkov-16021990 <zx84058405@live.com>
2023-11-01 20:54:08 +0300

    final first webPage HW

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-
-const myData =  {
-
-}
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.posters__title').classList.add('tracking-in-expand');
+    document.querySelector('.posters__button').classList.add('vibrate-1');
+});
